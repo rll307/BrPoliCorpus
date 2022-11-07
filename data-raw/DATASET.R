7c4136d4083672cbd55984eb137b97d95828b193
Rodrigo Esteves de Lima Lopes <rll307@unicamp.br>
2022-11-07 14:29:05 -0300

    Frist commit

---
+++ data-raw/DATASET.R
@@ -0,0 +1,11 @@
+## code to prepare `DATASET` dataset goes here
+
+
+library(readr)
+
+InauguralSpeeches <- read_csv("data-raw/InauguralSpeeches.csv")
+
+
+usethis::use_data(InauguralSpeeches, overwrite = TRUE)
+
+
