6439272a10b05e5fff03eb614abddb21e9197c69
rll307 <rll307@unicamp.br>
2024-07-24 19:41:39 +0200

    Update Doc

---
+++ doc/BrPoliCorpus.R
@@ -0,0 +1,12 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(BrPoliCorpus)
+
+## ----eval=FALSE, echo=TRUE----------------------------------------------------
+#  Brasil_2024 <- download_Brasil_president_2014()
+
